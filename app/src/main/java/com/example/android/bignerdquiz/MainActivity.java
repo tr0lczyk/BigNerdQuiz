156577456ee644e2bb302404a918787801fba464
Mateusz <30321069+tr0lczyk@users.noreply.github.com>
2018-08-29 15:00:41 +0200

    previous button

--- app/src/main/java/com/example/android/bignerdquiz/MainActivity.java
+++ app/src/main/java/com/example/android/bignerdquiz/MainActivity.java
@@ -5,39 +5,93 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button trueButton;
     private Button falseButton;
+    private Button nextButton;
+    private TextView questionTextView;
+
+    private Question[] questionBank = new Question[]{
+            new Question(R.string.question_canberra,true),
+            new Question(R.string.question_ocean,true),
+            new Question(R.string.question_mideast,false),
+            new Question(R.string.question_africa,false),
+            new Question(R.string.question_america,true),
+            new Question(R.string.question_asiaa,true)
+    };
+    private int currentIndex = 0;
+    private Button previousButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        questionTextView = findViewById(R.id.question_text_view);
+        questionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateQuestion();
+            }
+        });
 
         trueButton = findViewById(R.id.true_button);
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-               Toast toast =  Toast.makeText(MainActivity.this, getText(R.string.correct_toast),
-                        Toast.LENGTH_SHORT);
-               toast.setGravity(Gravity.CENTER,0,0);
-               toast.show();
+                checkAnswer(true);
             }
         });
-
 
         falseButton = findViewById(R.id.false_button);
         falseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Toast.makeText(MainActivity.this, getText(R.string.incorrect_toast),
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
+        nextButton = findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentIndex = (currentIndex + 1) % questionBank.length;
+                updateQuestion();
+            }
+        });
+
+        previousButton = findViewById(R.id.prvious_button);
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentIndex = (currentIndex - 1);
+                if(currentIndex < 0){
+                    currentIndex = questionBank.length-1;
+                }
+                updateQuestion();
+            }
+        });
+
+
+        updateQuestion();
+    }
+
+    private void updateQuestion(){
+        int question = questionBank[currentIndex].getTextResId();
+        questionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue){
+        boolean answerIsTrue = questionBank[currentIndex].isAnswerTrue();
+        int messageResId;
+        if(userPressedTrue == answerIsTrue){
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(MainActivity.this, messageResId, Toast.LENGTH_LONG).show();
     }
 }
